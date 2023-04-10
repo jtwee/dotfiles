4b545ec7917ccd25a23eae9d311885ce51f280f7
Jeremy Tweddle <jeremy.tweddle@shopify.com>
2023-04-10 09:42:02 -0400

    Add more useful repo aliases

--- dot_oh-my-zsh/custom/50-shopify.zsh
+++ dot_oh-my-zsh/custom/50-shopify.zsh
@@ -2,6 +2,8 @@
 alias dcd='dev cd'
 alias osui='dev cd online-store-ui'
 alias osw='dev cd online-store-web'
+alias web='dev cd web'
+alias shop='dev cd shopify'
 
 if [[ $SPIN -ne 1 ]]; then
   alias ds='dev server'
