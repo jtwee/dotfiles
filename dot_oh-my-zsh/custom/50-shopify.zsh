e02a9c9f495f833e58e9f28eff4a5c4072571250
Jeremy Tweddle <jeremy.tweddle@shopify.com>
2023-06-28 16:28:25 -0400

    Remove old dev aliases

--- dot_oh-my-zsh/custom/50-shopify.zsh
+++ dot_oh-my-zsh/custom/50-shopify.zsh
@@ -5,16 +5,3 @@ alias osw='dev cd online-store-web'
 alias sfr='dev cd storefront-renderer'
 alias shp='dev cd shopify'
 alias web='dev cd web'
-
-if [[ $SPIN -ne 1 ]]; then
-  alias ds='dev server'
-  alias da='dev all'
-  alias dup='dev up'
-  alias dups='dev up && dev server'
-  alias dupa='dev up && dev all'
-  alias ddup='dev down && dev up'
-  alias ddups='dev down && dev up && dev server'
-  alias ddupa='dev down && dev up && dev all'
-  alias ddupup='dev down && dev update && dev up'
-  alias ddown='dev down'
-fi
