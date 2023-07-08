31e467ad44be296148767720cc76ace78d9f03a9
Jeremy Tweddle <jeremy.tweddle@shopify.com>
2023-07-07 20:04:25 -0400

    Add zsh autosuggestions

--- dot_oh-my-zsh/custom/10-defaults.zsh
+++ dot_oh-my-zsh/custom/10-defaults.zsh
@@ -1,5 +1,6 @@
 DISABLE_AUTO_TITLE="true"
 
+setopt hist_expire_dups_first
 setopt hist_ignore_all_dups
 
 if [[ -n $SSH_CONNECTION ]]; then
@@ -11,3 +12,17 @@ fi
 export LANG="en_US.UTF-8"
 export LANGUAGE=$LANG
 export LC_ALL="en_US.UTF-8"
+
+KEYTIMEOUT=10
+bindkey -M menuselect '\e' send-break
+bindkey '^ ' autosuggest-accept
+
+_esc_to_clear_things () {
+  zle kill-line
+  zle autosuggest-clear
+}
+zle -N _esc_to_clear_things
+bindkey '\e' _esc_to_clear_things
+
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(bracketed-paste menu-complete expand-or-complete)
+ZSH_AUTOSUGGEST_HISTORY_IGNORE="cd *"
